3e5d4814bd8f7087f8d779e91fd959cb92f75344
Lilian <lilian.l.yuan@gmail.com>
2020-01-01 00:17:29 -0500

    Documentation for matrix and vector operations

--- matrix_vector_operations.h
+++ matrix_vector_operations.h
@@ -8,15 +8,45 @@ class Vector;
 
 class LinearAlgebraObject {
     public:
+        // Class constructor
         LinearAlgebraObject();
+
+        // Class destructor
         ~LinearAlgebraObject();
 
+        // transpose(m) returns the transpose of m
+        // effects: allocates memory (must call destructor on 
+        //   returned pointer)
         Matrix * transpose(Matrix * m);
+
+        // matrix_vector_multiply(m, v) returns the vector 
+        //   resulting from multiplying m with v
+        // effects: allocates memory (must call destructor on
+        //   returned pointer)
+        // requires: the length of v is equal to the number of
+        //   columns in m
         Vector * matrix_vector_multiply(Matrix * m, Vector * v);
+
+        // matrix_matrix_multiply(m1, m2) returns the matrix
+        //   resulting from multiplying m1 with m2 (m1 * m2)
+        // effects: allocates memory (must call destructor on 
+        //   returned pointer)
+        // requires: the number of columns in m1 is equal to the
+        //   number of rows in m2
         Matrix * matrix_matrix_multiply(Matrix * m1, Matrix * m2);
 
     private:
+        // ith_column_as_vector(m, i) returns the ith column of m 
+        //   as a vector
+        // effects: allocates memory (must call destructor on 
+        //   the returned pointer)
         Vector * ith_column_as_vector(Matrix * m, int i);
+
+        // map_vector_to_ith_column(m, v, i) maps the values of v
+        //   to the ith column of m
+        // effects: modifies m
+        // requires: the length of v is equal to the number of rows
+        //   in m
         void map_vector_to_ith_column(Matrix * m, Vector * v, int i);
 };
 
@@ -28,8 +58,16 @@ class Matrix : public LinearAlgebraObject {
         int num_rows;
         int num_cols;
 
+        // Class constructor. Sets num_rows to rows and num_cols 
+        //   to cols.
         Matrix(int rows, int cols);
+    
+        // Class destructor
         ~Matrix();
+
+        // get_matrix_entry(row, col) returns the value of the entry
+        //   in the m-th row and n-th column, where m and n are row 
+        //   and col respectively.
         float get_matrix_entry(int row, int col);
 };
 
@@ -39,7 +77,10 @@ class Vector : public LinearAlgebraObject {
         float * vals;
         int len;
 
+        // Class constructor. Sets len to use_len
         Vector(int use_len);
+
+        // Class destructor
         ~Vector();
 };
 
--- within.h
+++ within.h
@@ -2,6 +2,9 @@
 #define WITHIN_H
 
 
+// within(a, b) returns true if a and b are within 0.0001 of each 
+//   other, and false otherwise
+
 bool within(float a, float b);
 
 
