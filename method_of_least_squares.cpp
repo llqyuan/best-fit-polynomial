4ad7cc123af9e81d1c5d82d8071359ad4575f041
Lilian <lilian.l.yuan@gmail.com>
2020-01-02 17:34:19 -0500

    Method of least squares

---
+++ method_of_least_squares.cpp
@@ -0,0 +1,45 @@
+#include "method_of_least_squares.h"
+#include "matrix_vector_operations.h"
+#include "reducing.h"
+#include <cassert>
+
+
+using namespace std;
+
+
+MethodOfLeastSquares :: MethodOfLeastSquares() {}
+
+
+MethodOfLeastSquares :: ~MethodOfLeastSquares() {}
+
+
+MathVector * MethodOfLeastSquares :: least_squares_solution(Matrix * X,
+                                                            MathVector * y)
+{
+    Reducing * r = Reducing();
+    Matrix * X_transpose = X->transpose(X);
+    Matrix * X_transpose_times_X = X->matrix_matrix_multiply(
+        X_transpose, X);
+    MathVector * X_transpose_times_y = X->matrix_vector_multiply(
+        X_transpose, y);
+    r->reduce(X_transpose_times_X, X_transpose_times_y);
+    verify_identity_matrix(X_transpose_times_X);
+    delete r;
+    delete X_transpose;
+    delete X_transpose_times_X;
+    return X_transpose_times_y;
+}
+
+
+void MethodOfLeastSquares :: verify_identity_matrix(Matrix * m)
+{
+    assert(m->num_cols == m->num_rows);
+    const int dim = m->num_cols;
+    for (int i = 0; i < dim * dim; ++i) {
+        if (i / dim == i % dim) {
+            assert(within(m->get_matrix_entry(i / dim, i % dim), 1));
+        } else {
+            assert(within(m->get_matrix_entry(i / dim, i % dim), 0));
+        }
+    }
+}
