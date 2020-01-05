bbd907f4f7d62c46b7d8b9fe918bbf88c6dfd562
Lilian <lilian.l.yuan@gmail.com>
2020-01-05 14:48:57 -0500

    Organization of main file

--- main.cpp
+++ main.cpp
@@ -19,94 +19,26 @@ struct posn {
 
 // entry_exists(plist, x) returns true if x exists as an
 //   x coordinate in plist and false otherwise
-bool entry_exists(vector<posn *> * plist, float x) 
-{
-    for (int i = 0; i < plist->size(); ++i)
-    {
-        if (within(x, (plist->at(i))->x)) 
-        {
-            return true;
-        }
-    }
-    return false;
-}
+bool entry_exists(vector<posn *> * plist, float x);
 
 
 // least_squares_matrix(plist, degree) returns the matrix X in
 //   the normal system (X^T)*X*a = (X^T)*y
 // effects: allocates memory (must call destructor on returned
 //   pointer)
-Matrix * least_squares_matrix(vector<posn *> * plist, const int degree)
-{
-    Matrix * m = new Matrix(plist->size(), degree + 1);
-    for (int row = 0; row < plist->size(); ++row)
-    {
-        float x = (plist->at(row))->x;
-        for (int col = 0; col < m->num_cols; ++col)
-        {
-            m->vals[row * m->num_cols + col] = pow(x, col);
-        }
-    }
-    return m;
-}
+Matrix * least_squares_matrix(vector<posn *> * plist, const int degree);
 
 
 // least_squares_vector(plist) returns the vector  y in the
 //   normal system (X^T)*X*a = (X^T)*y
 // effects: allocates memory (must call destructor on returned
 //   pointer)
-MathVector * least_squares_vector(vector<posn *> * plist)
-{
-    MathVector * v = new MathVector(plist->size());
-    for (int i = 0; i < plist->size(); ++i)
-    {
-        float y = (plist->at(i))->y;
-        v->vals[i] = y;
-    }
-    return v;
-}
+MathVector * least_squares_vector(vector<posn *> * plist);
 
 
 // pretty_print_polynomial(v) prints v as a polynomial
 // effects: prints output
-void pretty_print_polynomial(MathVector * v)
-{
-    if (v->len) {
-        cout << "f(x) = ";
-        bool print_plus = false;
-        for (int i = 0; i < v->len; ++i) {
-            if (within(v->vals[i], 0)) {
-                continue;
-            }
-
-            if (print_plus && v->vals[i] > 0) {
-                cout << " + ";
-            } else if (print_plus && v->vals[i] < 0) {
-                cout << " - ";
-            }
-            print_plus = true;
-
-            if (i == 0) {
-                cout << v->vals[0];
-
-            } else if (i == 1 && within(fabs(v->vals[i]), 1)) {
-                cout << "x";
-
-            } else if (i == 1) {
-                cout << fabs(v->vals[i]) << "x";
-
-            } else if (within(fabs(v->vals[i]), 1)) {
-                cout << "x^" << i;
-
-            } else {
-                cout << fabs(v->vals[i]) << "x^" << i;
-            }
-        }
-    } else {
-        cout << "f(x) = 0";
-    }
-    cout << endl;
-}
+void pretty_print_polynomial(MathVector * v);
 
 
 // Used to store constants for keeping track of different statuses 
@@ -284,4 +216,87 @@ int main(void)
     for (int i = 0; i < posn_list.size(); ++i) {
         delete posn_list.at(i);
     }
+}
+
+
+// ============================================================
+
+
+bool entry_exists(vector<posn *> * plist, float x) 
+{
+    for (int i = 0; i < plist->size(); ++i)
+    {
+        if (within(x, (plist->at(i))->x)) 
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+Matrix * least_squares_matrix(vector<posn *> * plist, const int degree)
+{
+    Matrix * m = new Matrix(plist->size(), degree + 1);
+    for (int row = 0; row < plist->size(); ++row)
+    {
+        float x = (plist->at(row))->x;
+        for (int col = 0; col < m->num_cols; ++col)
+        {
+            m->vals[row * m->num_cols + col] = pow(x, col);
+        }
+    }
+    return m;
+}
+
+
+MathVector * least_squares_vector(vector<posn *> * plist)
+{
+    MathVector * v = new MathVector(plist->size());
+    for (int i = 0; i < plist->size(); ++i)
+    {
+        float y = (plist->at(i))->y;
+        v->vals[i] = y;
+    }
+    return v;
+}
+
+
+void pretty_print_polynomial(MathVector * v)
+{
+    if (v->len) {
+        cout << "f(x) = ";
+        bool print_plus = false;
+        for (int i = 0; i < v->len; ++i) {
+            if (within(v->vals[i], 0)) {
+                continue;
+            }
+
+            if (print_plus && v->vals[i] > 0) {
+                cout << " + ";
+            } else if (print_plus && v->vals[i] < 0) {
+                cout << " - ";
+            }
+            print_plus = true;
+
+            if (i == 0) {
+                cout << v->vals[0];
+
+            } else if (i == 1 && within(fabs(v->vals[i]), 1)) {
+                cout << "x";
+
+            } else if (i == 1) {
+                cout << fabs(v->vals[i]) << "x";
+
+            } else if (within(fabs(v->vals[i]), 1)) {
+                cout << "x^" << i;
+
+            } else {
+                cout << fabs(v->vals[i]) << "x^" << i;
+            }
+        }
+    } else {
+        cout << "f(x) = 0";
+    }
+    cout << endl;
 }
